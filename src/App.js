b656fe28cdedf2099e1273fc9881a3772d779cf4
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-13 10:42:44 +0800

    padding data via props, reset expense item css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenseItems = generateExpenseItems();
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem/>
+      <ExpenseItem
+        date={expenseItems[0].date}
+        title={expenseItems[0].title}
+        amount={expenseItems[0].amount}
+      />
+      <ExpenseItem
+        date={expenseItems[1].date}
+        title={expenseItems[1].title}
+        amount={expenseItems[1].amount}
+      />
+      <ExpenseItem
+        date={expenseItems[2].date}
+        title={expenseItems[2].title}
+        amount={expenseItems[2].amount}
+      />
     </div>
   );
+}
+
+function generateExpenseItems() {
+  return [
+    {
+      id: 100,
+      date: new Date(2023, 2, 13),
+      title: 'Hair Clay',
+      amount: 20
+    },
+    {
+      id: 101,
+      date: new Date(2023, 2, 12),
+      title: 'Jeans',
+      amount: 30
+    },
+    {
+      id: 102,
+      date: new Date(2023, 2, 11),
+      title: 'Adidas Stan Smith',
+      amount: 70
+    }
+  ];
 }
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 2, 13);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
-
+function ExpenseItem({date, title, amount}) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
