be122fea6f7d76f9429fc74d7a6191560fbba7e2
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-19 19:18:09 +0800

    add card common components, prevent default form submit event

--- src/add-user/UserForm.js
+++ src/add-user/UserForm.js
@@ -1,20 +1,27 @@
 import styles from './UserForm.module.css';
+import Card from '../shared/card';
 import Button from '../shared/button';
 
 export default function UserForm() {
   return (
-    <form className={styles.form}>
-      <div className={styles['form--row']}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text"/>
-      </div>
-      <div className={styles['form--row']}>
-        <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="text"/>
-      </div>
-      <div className={styles['form--row']}>
-        <Button type="submit">Add User</Button>
-      </div>
-    </form>
+    <Card className={styles['form-container']}>
+      <form className={styles.form} onSubmit={event => submitHandler(event)}>
+        <div className={styles['form--row']}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text"/>
+        </div>
+        <div className={styles['form--row']}>
+          <label htmlFor="age">Age (Years)</label>
+          <input id="age" type="text"/>
+        </div>
+        <div className={styles['form--row']}>
+          <Button type="submit">Add User</Button>
+        </div>
+      </form>
+    </Card>
   );
 };
+
+function submitHandler(event) {
+  event.preventDefault();
+}
