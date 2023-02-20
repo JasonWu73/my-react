54fd4ac43550ea5210b71afa70c99c1e729e9b4e
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-20 10:32:25 +0800

    validation form

--- src/add-user/UserForm.js
+++ src/add-user/UserForm.js
@@ -1,18 +1,71 @@
+import {useState} from 'react';
+
 import styles from './UserForm.module.css';
 import Card from '../shared/card';
 import Button from '../shared/button';
 
 export default function UserForm() {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+  const [isInvalidEntered, setIsInvalidEntered] = useState(false);
+
+  const usernameChangeHandler = event => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = event => {
+    setEnteredAge(event.target.value);
+  };
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+    const isEmptyEntered =
+      enteredUsername.trim().length === 0 || enteredAge.trim().length === 0;
+    const isInvalidAge = +enteredAge < 0 || isNaN(+enteredAge);
+    if (isEmptyEntered || isInvalidAge) {
+      setIsInvalidEntered(true);
+      return;
+    }
+
+    setIsInvalidEntered(false);
+    console.log(enteredUsername, +enteredAge);
+
+    setEnteredUsername('');
+    setEnteredAge('');
+  };
+
   return (
     <Card className={styles['form-container']}>
-      <form className={styles.form} onSubmit={event => submitHandler(event)}>
-        <div className={styles['form--row']}>
+      <form
+        className={styles.form}
+        onSubmit={
+          event => submitHandler(
+            event,
+            enteredUsername,
+            setEnteredUsername,
+            enteredAge,
+            setEnteredAge
+          )
+        }
+      >
+        <div className={`${styles['form--row']} ${isInvalidEntered ? styles.error : ''}`}>
           <label htmlFor="username">Username</label>
-          <input id="username" type="text"/>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
         </div>
-        <div className={styles['form--row']}>
+          <div className={`${styles['form--row']} ${isInvalidEntered ? styles.error : ''}`}>
           <label htmlFor="age">Age (Years)</label>
-          <input id="age" type="text"/>
+          <input
+            id="age"
+            type="text"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
         </div>
         <div className={styles['form--row']}>
           <Button type="submit">Add User</Button>
@@ -21,7 +74,3 @@ export default function UserForm() {
     </Card>
   );
 };
-
-function submitHandler(event) {
-  event.preventDefault();
-}
